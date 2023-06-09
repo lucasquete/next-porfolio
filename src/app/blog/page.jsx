36eb8011791fbf8defb07f1384da4ef173d87070
Javier773 <javimoreno20@hotmail.com>
2023-06-09 16:02:25 +0200

    fix

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 async function getData() {
-  const res = await fetch("https://next-porfolio-nu.vercel.app/api/posts", {
+  const res = await fetch(`${process.env.VERCEL_URL}/api/posts`, {
     cache: "no-cache",
   });
 
