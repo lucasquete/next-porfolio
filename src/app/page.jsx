8781579fa0ac1b0c11abceaa5592dee97430b075
Javier773 <javimoreno20@hotmail.com>
2023-06-09 14:11:06 +0200

    fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,8 @@ import styles from './page.module.css'
 import Hero from "public/hero.png"
 import Button from '@/components/button/Button'
 
+// main page
+
 export default function Home() {
   return (
     <div className={styles.container}>
