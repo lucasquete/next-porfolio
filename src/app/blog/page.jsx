4b20e739e728d85eaa17573f047514ec9946ff13
Javier773 <javimoreno20@hotmail.com>
2023-06-09 17:50:45 +0200

    fix

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import styles from "./blog.module.css"
 import Link from 'next/link'
 import Image from 'next/image'
+import useSWR from "swr"
 
-async function getData() {
-  const res = await fetch(`https://${VERCEL_URL}/api/posts`, {
-    cache: "no-cache",
-  });
+// async function getData() {
+//   const res = await fetch(`https://localhost:3000/api/posts`, {
+//     cache: "no-cache",
+//   });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data!");
-  }
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data!");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
-const Blog = async () => {
+const Blog = () => {
 
-  const data = await getData();
+  // const data = await getData();
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, isLoading } = useSWR(`/api/posts`, fetcher);
 
   return (
     <div className={styles.mainContainer}>
-      {data.map((item) => (
+      {isLoading ? (<h1>Loading</h1>) : data.map((item) => (
         <Link href={`/blog/${item?._id}`} className={styles.container} key={item?._id}>
           <div className={styles.imageContainer}>
             <Image
