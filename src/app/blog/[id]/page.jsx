af45f9b6d5b2eaacc0b0027f2b8d735a1326fec4
Javier773 <javimoreno20@hotmail.com>
2023-06-09 19:48:39 +0200

    fix

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { notFound } from 'next/navigation';
 
 async function getData(id) {
-  const res = await fetch(`https://next-porfolio-nu.vercel.app/api/posts/${id}`, {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
     cache: "no-store",
   });
 
